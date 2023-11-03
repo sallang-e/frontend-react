7a02b670cf108cb08f14e69ce8187a75f308290c
깃짱 <107979804+eunkeeee@users.noreply.github.com>
2023-11-04 00:56:24 +0900

    feat: 로그인 버튼 찐퉁으로 가져오기 (#2)
    * feat: footer 추가
    
    * feat: local changes
    
    * feat: 카카오 로그인 버튼 만들기
    
    * feat: 구글, 페이스북 로그인 버튼 찐퉁으로 데려오기

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,16 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 
 function Footer() {
-    return (
-        <>
-            <div>footer</div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <div>주변 자전거</div>
+        <div>한강 이벤트</div>
+        <div>내 이용내역</div>
+      </div>
+      <div>footer</div>
+    </>
+  );
 }
 
 export default Footer;
