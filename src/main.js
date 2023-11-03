43ae2d1e4a73fa8b1c2f0bf8ab8fa4fcd8d83c76
깃짱 <107979804+eunkeeee@users.noreply.github.com>
2023-11-04 02:24:15 +0900

    feat: footer 설정 (#4)

--- src/main.js
+++ src/main.js
@@ -1,28 +1,27 @@
 import Header from "./components/header";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import MyPage from "./components/mypage";
 import Map from "./components/map";
-import Footer from "./components/footer";
-
+import footer from "./components/footer";
 
 function Main() {
-    const page = useSelector((state) => state.page);
+  const page = useSelector((state) => state.page);
 
-    if(page == 1 ){
-        return (
-            <div className="body">
-                <Header/>
-                <Map/>
-                <Footer/>
-            </div>
-        )
-    }else if(page == 2) {
-        return (
-            <div className="body">
-                <MyPage/>
-            </div>
-        )
-    }
+  if (page == 1) {
+    return (
+      <div className="body">
+        <Header />
+        <Map />
+        <footer />
+      </div>
+    );
+  } else if (page == 2) {
+    return (
+      <div className="body">
+        <MyPage />
+      </div>
+    );
+  }
 }
 
 export default Main;
